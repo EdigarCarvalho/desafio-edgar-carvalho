ddc735fb3405b40260f9f92a5c0dea29203c60ff
EdigarCarvalho <edgarcarvalho8989@gmail.com>
2023-08-12 22:02:38 -0300

    fix: CaixaDaLanchonete class methods

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,4 +1,4 @@
-let preco = {
+let PRECOS = {
     cafe: 3,
     chantily: 1.5,
     suco: 6.2,
@@ -9,7 +9,7 @@ let preco = {
     combo2: 7.5,
 };
 
-let adicionais = {
+let ADICIONAIS = {
     chantily: "cafe",
     queijo: "sanduiche",
 }
@@ -20,45 +20,68 @@ class CaixaDaLanchonete {
         if(itens.length === 0) 
             return "Não há itens no carrinho de compra!";
 
-        let precoTotal = 0;
+        let precoTotalBase = this.calcularPrecoTotalBase(itens);
+        if(precoTotalBase.hasOwnProperty("error")) 
+            return precoTotalBase.error;
+
+        precoTotalBase = this.aplicarDesconto( precoTotalBase , metodoDePagamento );
+        if(precoTotalBase.hasOwnProperty("error")) 
+            return precoTotalBase.error;   
+
+        return `R$ ${precoTotalBase.toFixed(2).replace('.', ',')}`;
+    }
+
+    calcularPrecoTotalBase(itens){
+
+        let precoTotalBase = 0;
         let produtos = [];
 
         for (const item of itens) {
             const itemParts = item.split(',');
             const produto = itemParts[0];
             
-            if ( !preco.hasOwnProperty(produto) ) return "Item inválido!";
-            else if ( adicionais[produto] ){
-                const principal = adicionais[produto];
-                if( !produtos.includes(principal) ) return "Item extra não pode ser pedido sem o principal"
-            }
-            
+            const error = this.validarProduto( produto, produtos)
+            if(error) return error ;
+
             produtos.push(produto);
 
             const quantidade = parseInt(itemParts[1]);
 
-            if ( quantidade <= 0 ) return "Quantidade inválida!"
+            if ( quantidade <= 0 ) return { error: "Quantidade inválida!" } ;
 
-            precoTotal += preco[produto] * quantidade;
+            precoTotalBase += PRECOS[produto] * quantidade;
+
+            }
+
+            return precoTotalBase;
+    }
+
+    validarProduto(produto , produtos){
+        if ( !PRECOS.hasOwnProperty(produto) )
+             return { error: "Item inválido!" };
+
+        else if ( ADICIONAIS[produto] ){
+            const principal = ADICIONAIS[produto];
+
+            if( !produtos.includes(principal) )
+                return { error: "Item extra não pode ser pedido sem o principal"} ;
         }
 
+        return;
+    }
 
+    aplicarDesconto(precoTotalBase, metodoDePagamento){
         switch(metodoDePagamento){
             case "debito":
-                break; 
+                return precoTotalBase; 
             case "dinheiro":
-                precoTotal -= precoTotal * 0.05
-                break;
+                return precoTotalBase - precoTotalBase * 0.05;
             case "credito":
-                precoTotal += precoTotal * 0.03
-                break;
+                return precoTotalBase + precoTotalBase * 0.03;
             default:    
-                return "Forma de pagamento inválida!"      
+                return { error: "Forma de pagamento inválida!" };      
         }
-
-        return `R$ ${precoTotal.toFixed(2).replace('.', ',')}`;
-
-        }
+    }
 }
 
 export { CaixaDaLanchonete };
